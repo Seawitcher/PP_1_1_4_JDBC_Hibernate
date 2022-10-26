14e19e003ee34525e2b400ee48f0a52f6ba1d78b
Seawitcher <Seawitcher83@yandex.ru>
2022-10-26 12:45:30 +0400

    Version 0.5 (Added practice task 1.1.4)

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -2,10 +2,17 @@ package jm.task.core.jdbc.dao;
 
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
 
 import java.util.List;
 
 public class UserDaoHibernateImpl implements UserDao {
+    private final SessionFactory sessionFactory = Util.getSessionFactory();
+
     public UserDaoHibernateImpl() {
 
     }
@@ -13,31 +20,73 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void createUsersTable() {
+        Session session = sessionFactory.openSession();
+
+        final String hql = "CREATE TABLE if not exists `users` (\n" +
+                "  `id` int NOT NULL AUTO_INCREMENT,\n" +
+                "  `name` varchar(45) NOT NULL,\n" +
+                "  `last_name` varchar(45) NOT NULL,\n" +
+                "  `age` int NOT NULL,\n" +
+                "  PRIMARY KEY (`id`)\n" +
+                ") ";
+        session.beginTransaction();
+        Query query = session.createSQLQuery(hql);
+        query.executeUpdate();
+        session.getTransaction().commit();
 
     }
 
     @Override
     public void dropUsersTable() {
-
+        Session session = sessionFactory.openSession();
+        final String hql = "DROP TABLE IF EXISTS users;";
+        session.beginTransaction();
+        Query query =session.createSQLQuery(hql);
+        query.executeUpdate();
+        session.getTransaction().commit();
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
+        Session session =sessionFactory.openSession();
+
+        User user = new User(name,lastName,age);
+        session.beginTransaction();
+        session.save(user);
+        session.getTransaction().commit();
 
     }
 
     @Override
     public void removeUserById(long id) {
+        Session session = sessionFactory.openSession();
+        User user = session.get(User.class, id);
+        session.beginTransaction();
+        session.delete(user);
+        session.getTransaction().commit();
+
 
     }
 
     @Override
     public List<User> getAllUsers() {
-        return null;
+
+        Session session = sessionFactory.openSession();
+        String hql = "SELECT u FROM User u";
+        session.getTransaction();
+        Query query = session.createQuery(hql, User.class);
+
+        return  (List<User>) query.list();
     }
 
     @Override
     public void cleanUsersTable() {
+        Session session = sessionFactory.openSession();
+        final String sql = "TRUNCATE TABLE users;";
+        session.beginTransaction();
+        Query query = session.createSQLQuery(sql);
+        query.executeUpdate();
+        session.getTransaction().commit();
 
     }
 }
--- src/main/java/jm/task/core/jdbc/model/User.java
+++ src/main/java/jm/task/core/jdbc/model/User.java
@@ -1,9 +1,6 @@
 package jm.task.core.jdbc.model;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.util.Objects;
 
 @Entity
@@ -11,15 +8,17 @@ import java.util.Objects;
 
 public class User {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
 
-    @Column
+    @Column(name = "name")
     private String name;
 
-    @Column
+    @Column(name = "last_name")
     private String lastName;
 
-    @Column
+    @Column (name = "age")
     private Byte age;
 
     public User() {
