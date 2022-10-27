34f56abf653f9af7f1d827806b133bd06215e391
Seawitcher <Seawitcher83@yandex.ru>
2022-10-27 14:15:48 +0400

    Version 0.7 (Connection deleted)

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -6,11 +6,7 @@ import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.cfg.Environment;
 import org.hibernate.service.ServiceRegistry;
-import java.sql.Connection;
 
-
-import java.sql.DriverManager;
-import java.sql.SQLException;
 import java.util.Properties;
 
 public class Util {
@@ -42,24 +38,6 @@ public class Util {
         return sessionFactory;
     }
 
-    private static final String DB_DRIVER = "com.mysql.jdbc.Driver";
-    private static final String URL = "jdbc:mysql:// localhost:3306/my_db";
-    private static final String USER = "root";
-    private static final String PASS = "root";
-    public static Connection getConnection() {
-        Connection connection = null;
-
-        try {
-            Class.forName(DB_DRIVER);
-            connection = DriverManager.getConnection(URL, USER, PASS);
-            System.out.println("Connection - Ok!");
-        }
-        catch (ClassNotFoundException | SQLException e) {
-            e.printStackTrace();
-            System.out.println("Connection - Error!");
-        }
-        return null;
-    }
 }
 
 
